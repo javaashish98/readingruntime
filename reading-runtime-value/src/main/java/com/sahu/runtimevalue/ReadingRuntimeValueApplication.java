9e00705a79d05bdd51bee60d631c780cb35ba974
Ashish <Ashish@DESKTOP-5NHOKSN>
2025-02-12 07:56:38 +0530

    added few properties

--- reading-runtime-value/src/main/java/com/sahu/runtimevalue/ReadingRuntimeValueApplication.java
+++ reading-runtime-value/src/main/java/com/sahu/runtimevalue/ReadingRuntimeValueApplication.java
@@ -1,21 +1,28 @@
 package com.sahu.runtimevalue;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.sahu.runtimevalue.service.MyCustomService;
 
 @SpringBootApplication
-public class ReadingRuntimeValueApplication {
+public class ReadingRuntimeValueApplication implements ApplicationRunner{
 
+	@Autowired
+	private MyCustomService service;
+	
 	public static void main(String[] args) {
-		
-		MyCustomService service = new MyCustomService();
-		
-		
 		SpringApplication.run(ReadingRuntimeValueApplication.class, args);
-		
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
 		service.printProjectName();
+		
 	}
 
 }
--- reading-runtime-value/src/main/java/com/sahu/runtimevalue/service/MyCustomService.java
+++ reading-runtime-value/src/main/java/com/sahu/runtimevalue/service/MyCustomService.java
@@ -1,15 +1,17 @@
 package com.sahu.runtimevalue.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyCustomService {
 
-	
+	@Value("${projectName}")
+	private String projectName;
 	
 	public void printProjectName() {
-		String projectName = "Reading RunTime Value";
 		System.out.println("Project Name "+projectName);
+		
 	}
 	
 }
