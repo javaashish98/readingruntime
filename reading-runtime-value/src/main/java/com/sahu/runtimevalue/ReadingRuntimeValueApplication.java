a8f587082531e107b6da4a40505d220c59098501
Ashish <Ashish@DESKTOP-5NHOKSN>
2025-02-12 07:38:18 +0530

    initial commit

---
+++ reading-runtime-value/src/main/java/com/sahu/runtimevalue/ReadingRuntimeValueApplication.java
@@ -0,0 +1,13 @@
+package com.sahu.runtimevalue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingRuntimeValueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingRuntimeValueApplication.class, args);
+	}
+
+}
---
+++ reading-runtime-value/src/test/java/com/sahu/runtimevalue/ReadingRuntimeValueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahu.runtimevalue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingRuntimeValueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
